f03e6fe004b8b9ad57e8f0b308e5b166ffe2dbcd
ashtyyyxxx-main <147919956+ashtyyyxxx-main@users.noreply.github.com>
2024-08-25 10:49:05 +0700

    grabbing chickens

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,14 +1,11 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Enemy : MonoBehaviour
 {
     private UnityEngine.AI.NavMeshAgent agent;
-    Animator animator;
+    public Animator animator;
     public ParticleSystem hitParticles;
     [SerializeField] private ParticleSystem deathParticles;
     [SerializeField] private float enemyDamage;
